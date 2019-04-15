655dc909ecec2637312efc1d039633539cc0e174
thiagolam <tl-10@hotmail.com>
2019-04-15 16:41:56 -0300

    commit inicial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const Heroes: Hero[] = [
+    { id: 11, name: 'Mr.Niceman' },
+    { id: 12, name: 'Blastwoman' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Clerita' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'Rubberboy' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr.X' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornando' }
+];
